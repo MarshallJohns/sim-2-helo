61125b0d9bd826a20130fb1e96a90f5bb24cc6c4
Marshall Johnson <marshalljohnson193@gmail.com>
2020-09-15 11:54:03 -0600

    init style

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import './App.css';
-import Auth from './Components/Auth/Auth'
-import Dashboard from './Components/Dashboard/Dashboard'
-import Form from './Components/Form/Form'
 import Nav from './Components/Nav/Nav'
-import Post from './Components/Post/Post'
+import routes from './routes'
+import { withRouter } from 'react-router-dom'
 
-function App() {
+
+function App(props) {
+  const { pathname } = props.location
   return (
-    <div className="App">
-      <Nav />
-      <Auth />
-      <Dashboard />
-      <Form />
-      <Post />
-    </div>
+    <div className="App" >
+      {pathname !== '/' ? <Nav /> : null}
+      { routes}
+    </div >
   );
+
 }
 
-export default App;
+export default withRouter(App);
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Nav extends Component {
 
     render() {
+
         return (
-            <div>Nav.js</div>
+            <div className='navbar'>
+                <Link to='/dashboard'>Dashboard</Link>
+                <Link to='/new'>New post</Link>
+                <Link to='/'>Logout</Link>
+            </div>
         )
     }
 }
